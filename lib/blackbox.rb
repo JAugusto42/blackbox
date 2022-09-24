616ad1d904f35e909fc1f18209c952d3fc8a985e
Jose Augusto <joseaugusto.881@outlook.com>
2022-09-23 21:14:18 -0300

    initial gem configuration

---
+++ lib/blackbox.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "blackbox/version"
+
+module Blackbox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/blackbox/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Blackbox
+  VERSION = "0.1.0"
+end
